363d0235592f1ff9a3a2fd1d22deebac101489ee
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-08 22:03:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    return array[0]
+  end 
+  if array.size == 2 
+    return "#{array[0]} and #{array[1]}"
+  end 
+  if array.size > 2 
+    x = array.size 
+  array[x - 1] = "and #{array[x-1]}"
+  return array.join(", ")
+end 
 end
